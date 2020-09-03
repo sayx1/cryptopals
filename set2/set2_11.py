d0cd9de11ff3489f4ece09b8a38a577dc9d16e3f
x1 <x1@say.x1>
2020-09-03 22:55:38 +0545

    fixed up set2 challenge 11 with better detection wq!

--- set2/set2_11.py
+++ set2/set2_11.py
@@ -1,144 +1,260 @@
-import base64
-from Crypto.Cipher import AES
-import binascii
+import random 
 import string
-import random
-from binascii import a2b_hex
+import sys
+from Crypto.Cipher import AES
+
+def encryption_oracle(text:bytes)->bytes:
+    """ takes a bunch of text, adds a bunch of additional 
+        text, generates a random key and initilization
+        vector. then randomly encrypts it in ebc or cbc
 
 
-def randomkey(length):
-    stringLength= length
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(stringLength)).encode()
+    param1: text to be encrypted (bytes)
 
-def addtotext(text):
-    frontlen = random.randint(5,10)
-    backlen = random.randint(5,10)
-    letters = string.ascii_lowercase
-    front = ''
-    back = ''
-    for i in range(frontlen):
-        front += random.choice(letters)
-    for i in range(backlen):
-        back += random.choice(letters)
-    return (front + text + back)
+    rtype: encrypted text (bytes)
+    """
+
+    text = additional_text(text) 
+    for i in range(100):
+        key = randomkey(16)
+        #print(key)
+        iv =  randomkey(16)
+        #print(len(iv))
+        if len(key)==16 and len(iv) == 16:
+            break
+
+    n = random.randint(0,1)
+    print("n=",n)
     
 
-def decrypt_128_aes_ecb(text,key):
-    cipher = AES.new(key,AES.MODE_ECB)
-    plaintext = cipher.decrypt(bytes(text))
-    return plaintext
-
-def encrypt_128_aes_ecb(text,key):
-    cipher = AES.new(key,AES.MODE_ECB)
-    plaintext = cipher.encrypt(text)
-    return plaintext 
-
-def pkcs_padding(cipher,blocksize):
-    padding_len = blocksize - len(cipher)
-    for i in range(padding_len):
-        cipher = cipher + bytes([padding_len])
-    return(cipher)
-
-def chunks(text,blocksize):
-    chunks = []
-    for i in range(0,len(text),blocksize):
-        chunks.append(bytes(text[i:i+blocksize]))
-    return chunks
-
-def xor_chunks (text,iv):
-    ans = bytearray([_a ^ _b for _a, _b in zip(bytearray(text),bytearray(iv))])
-    return ans
-    
- 
-def encrypt_128_aes_cbc(text,key,iv):
-    chunk_text = chunks(text,16)
-    padded_text = [pkcs_padding(i,16) for i in chunk_text]
-    previous_chunk = iv
-    cipher_text = []
-    for i in padded_text:
-        xor_text = xor_chunks(i,previous_chunk)
-        cipher = encrypt_128_aes_ecb(xor_text,key)
-        cipher_text.append(cipher)
-        previous_chunk = cipher  
-
-    return cipher_text
-
-def decrypt_128_aes_cbc(text,key,iv):
-    chunk_text = chunks(text,16)
-    #padded_text = [pkcs_padding(i,16) for i in chunk_text]
-    previous_chunk = iv
-    plain_text = []
-    for i in text:  
-        decrypted = decrypt_128_aes_ecb(i,key)
-        plain_text.append(xor_chunks(decrypted,previous_chunk))
-       
-        previous_chunk = i
-
-    return plain_text
-
-def aes_encrypt(text,key):
-    chunk_text = chunks(text,16)
-    padded_text = [pkcs_padding(i,16) for i in chunk_text]
-    cipher = []
-    for i in padded_text: 
-        if (len(i)==16):
-         encrypted = encrypt_128_aes_ecb(i,key)
-         cipher.append(encrypted)
-    return cipher
-
-def aes_decrypt(text,key):
-    #chunk_text = chunks(text,16)
-    #padded_text = [pkcs_padding(i,16) for i in chunk_text]
-    cipher = []
-    for i in text: 
-        if (len(i)==16):
-         encrypted = decrypt_128_aes_ecb(i,key)
-         cipher.append(encrypted)
-    return cipher
-
-def encryption_oracle(text):
-    text = addtotext(text) 
-    key = randomkey(16)
-    #print(key)
-    iv =  randomkey(16)
-    #print(iv)
-
-    #n = random.randint(0,1)
-    n = 1 
-
-    if n:
+    if n == 1 :
         cipher = encrypt_128_aes_cbc(text,key,iv)
-        #plain = decrypt_128_aes_cbc(cipher,key,iv)
-        #print(cipher)
-        #print(plain)
-        f_cipher = b''
-        for i in cipher:
-             f_cipher += i
-        return(f_cipher)
-           
-    else:
-        cipher = aes_encrypt(text,key)
-        #plain = aes_decrypt(cipher,key)
-        #print(plain)
-        f_cipher = b''
-        for i in cipher:
-             f_cipher += i
-        return(f_cipher)
+        #plaintext = decrypt_128_aes_cbc(cipher,key,iv)
+        return(cipher)
+    
+    elif n == 0:
+        cipher = encrypt_aes_ecb(text,key)
+        #plaintext = decrypt_aes_ecb(cipher,key)
+        return(cipher)
 
 
-def detect_ebc(text):
-    bin_str = map(bin,bytearray(text))
-    repeat = len(bin_str)-len(set(bin_str))
+
+def find_repeating_blocks(cip: bytes) -> int:
+
+
+    chunk_tex = chunks(cip,16)
+    repeat = len(chunk_tex) - len(set(chunk_tex))
+    #for i in range(len(chunk_tex)):
+    #    for j in range(i+1,len(chunk_tex)):
+    #        if chunk_tex[i] == chunk_tex[j]:
+    #    
+    #            repeat = repeat + 1
+
     return(repeat)
 
+
+
+
+def encrypt_aes_ecb(text:bytes,key:bytes)->bytes:
+    """ takes a bunch of text and splits it in chunks, pads it in 
+        pkcs (#PKCS7)
+    param1 : text to be encrypted (bytes)
+    key : key to encrypt (bytes)
+
+    r_type : cipher text (bytes)
+
+    """
+
+    chunk_text = chunks(text,16)
+    padded_chunks = padded_text(chunk_text)
+    cipher_text = b''
+    for i in padded_chunks:
+    
+        cipher = encrypt_128_aes_ecb(i,key)
+        cipher_text += cipher
+    return cipher_text 
+
+
+
+def decrypt_aes_ecb(cipher_text:bytes,key:bytes)->bytes:
+    """ takes a bunch of cipher text and splits it in chunks and
+        returns plain text
+    param1 : text to be encrypted (bytes)
+    key : key to encrypt (bytes)
+
+    r_type : cipher text (bytes)
+
+    """
+
+    chunk_text = chunks(cipher_text,16)
+    plain_text = b''
+    for i in chunk_text:
+        '''
+        print("i",i)
+        print("previous",previous_chunk)
+        print("")
+        '''
+        plain = decrypt_128_aes_ecb(i,key)
+        plain_text = plain_text + plain
+    return plain_text
+
+
+         
+
+def encrypt_128_aes_cbc(text:bytes,key:bytes,iv:bytes)->bytes:
+    """function to take plaintext and encrypt it using aes 128 cbc 
+
+    param1 -> plaintext
+    param2 -> key to encrypt data
+    iv -> initilization vector
+
+    return cipher text 
+
+    """
+    chunk_text = chunks(text,16)
+    padded_chunks = padded_text(chunk_text)
+    previous_chunk = iv
+    cipher_text = b''
+    for i in padded_chunks:
+        xor_text = xor_chunks(i,previous_chunk)
+        cipher = encrypt_128_aes_ecb(xor_text,key)
+        previous_chunk = cipher
+        cipher_text += cipher
+    return cipher_text
+
+
+
+def decrypt_128_aes_cbc(cipher_text,key,iv):
+    chunk_text = chunks(cipher_text,16)
+    previous_chunk = iv
+    plain_text = b''
+    for i in chunk_text:
+        '''
+        print("i",i)
+        print("previous",previous_chunk)
+        print("")
+        '''
+        plain = decrypt_128_aes_ecb(i,key)
+        xor_text = xor_chunks(plain,previous_chunk)
+        plain_text = plain_text + xor_text
+        previous_chunk = i
+    return plain_text
+
+
+
+
+        
    
+def padding(msg:bytes, bsize:int)->bytes:
+    """ pad the message to the blocksize using the PKCS#7 padding scheme 
+    :param msg -> message to pad (bytes)
+    :param bsize -> the block size to use (int)
+
+    return padded message (bytes)
+    """
+
+    if bsize<2 and bsize>255:
+        raise ValueError
+
+    msg_len = len(msg)
+    pad_size = bsize - (msg_len % bsize)
+    pad_val = pad_size.to_bytes(1, sys.byteorder, signed=False)
+    padding = pad_val * pad_size
+    return(msg+padding)
 
 
 
 
-text = b"orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-for i in range(100):
-    cipher = encryption_oracle(text)
-    repeat = detect_ebc(cipher)
-    print(repeat)
+
+def randomkey(length:int)->bytes:
+    """
+    function returns a random key of given length
+    param1: length of key (int)
+    
+    return: key (bytes)
+    """
+    letters = b'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789'
+    key = b''
+    for i in range(length):
+        j = random.randint(1,62)
+        key = key + letters[j:j+1]
+    return key
+
+
+
+
+
+def additional_text(text:bytes)->bytes:
+    """appends a random number of letter to the front and back of the plain text
+    pram1 -> plain text data
+
+    rturn plain text with random letters front and back
+    """
+    frontlen = random.randint(5,10)
+    backlen = random.randint(5,10)
+    front = randomkey(frontlen)
+    back = randomkey(backlen)
+    return (front + text + back)
+
+
+
+
+
+
+def padded_text(chunk_text:bytes)->bytes:
+    
+    padded_chunks = []
+    for i in chunk_text:
+        if len(i) != 16:
+            padded_chunks.append(padding(i,16))
+        else:
+            padded_chunks.append(i)
+
+    return padded_chunks
+
+
+
+def xor_chunks(text:bytes,p_text:bytes)->bytes:
+    return bytes([_a ^ _b for _a, _b in zip(text, p_text)])
+
+
+
+
+
+def encrypt_128_aes_ecb(text:bytes,key:bytes)->bytes:
+    cipher = AES.new(key,AES.MODE_ECB)
+    cipher_text = cipher.encrypt(text)
+    return cipher_text
+
+
+
+def decrypt_128_aes_ecb(text:bytes,key:bytes)->bytes:
+    cipher = AES.new(key,AES.MODE_ECB)
+    plaintext = cipher.decrypt(text)
+    return plaintext
+
+
+
+def chunks(text:bytes,blocksize:int)->bytes:
+    """
+    pram1: whole encrypted messages 
+    pram2: amount of chunks needed 
+
+    return array of chunks of data
+    """
+    chunk_data = b''
+    chunks = []
+    for i in range(0,len(text),blocksize):
+        chunk_data = text[i:i+blocksize] 
+        chunks.append(chunk_data)
+    return chunks
+
+
+if __name__ == '__main__':
+    fhandle = open("text.txt","rb")
+    text = fhandle.read()
+    for i in range(100):
+        cipher = encryption_oracle(text)
+        repeat = find_repeating_blocks(cipher)
+        print(repeat)
+
