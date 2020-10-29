6dc12194e9f9202ec930b85230ffe8fceea3e1ff
x1 <x1@say.x1>
2020-10-30 00:57:31 +0545

    added set4 challenge31 && 32

---
+++ set4/challenge31/server.py
@@ -0,0 +1,52 @@
+import tornado.web
+import time
+from set4_28 import * 
+DELAY = 0.05
+key = b'YELLOW SUBMARINE'
+
+"""
+with help from
+https://github.com/wangray/matasano-crypto/blob/master/set4/timing_server.py
+
+"""
+def insecure_compare(filename,signature):
+    signature = signature.encode('ascii')
+    filename = filename.encode('ascii')
+    actual_signature = SHA_1(key+filename).encode('ascii')
+    for i in range(len(signature)):
+        if signature[i] != actual_signature[i]:
+            return False 
+        time.sleep(DELAY)
+    return True 
+    
+
+
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write('Nothing to show, move along...')
+
+class TestHandler(tornado.web.RequestHandler):
+    def get(self):
+        filename = self.get_argument("file", None, True)
+        signature = self.get_argument("signature", None, True)
+
+        self.write("filename: " + filename + "<br>")
+        self.write("signature " + signature + "<br>")
+
+        if not insecure_compare(filename, signature):
+            self.set_status(500)
+            self.finish("<html><body>HMAC Check failed</body></html>")
+        else:
+            self.write("HMAC Check OK")
+
+
+if __name__ == "__main__":
+    application = tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/test", TestHandler)
+    ])
+
+    application.listen(8080)
+    tornado.ioloop.IOLoop.current().start()
---
+++ set4/challenge32/timing_attack_s04_c32.py
@@ -0,0 +1,28 @@
+import requests
+
+
+hmac_guess = 'A'*40
+j = 0
+time_elapsed = {}
+for j in range(40):
+    time_elapsed = {}
+    for k in range(150):
+        for i in range(47,123):
+            hmac_guess = list(hmac_guess)
+            hmac_guess[j] = chr(i)
+            hmac_guess = ''.join(hmac_guess)
+            payload = {'file': 'hello.txt', 'signature': hmac_guess}
+            r = requests.get("http://localhost:8080/test", params = payload)
+            ms_delay = r.elapsed.total_seconds()*1000
+            if k == 0: 
+                time_elapsed[chr(i)] = (ms_delay)
+            else:
+                time_elapsed[chr(i)] += (ms_delay)
+    hmac_at_j = max(time_elapsed, key=time_elapsed.get)
+    hmac_guess = list(hmac_guess)
+    hmac_guess[j] = hmac_at_j
+    hmac_guess = ''.join(hmac_guess)
+    j = j + 1
+print(hmac_guess)
+
+
